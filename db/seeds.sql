75b3103c9f60be74a0d6ba4a4f8013fefa30bc23
hhsaito <hhsaito@users.noreply.github.com>
2017-01-27 18:59:52 -0600

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Royal with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Slayer Burger');
+INSERT INTO burgers (burger_name) VALUES ('Anthrax Burger');
